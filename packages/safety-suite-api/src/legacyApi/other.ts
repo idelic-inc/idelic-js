2c87964b51f7dc61eaf4b44ee6c455e3d3d0859f
Andrew Russell <russell@idelic.com>
2019-11-09 14:07:58 -0500

    Add `getApi` api.

--- packages/safety-suite-api/src/legacyApi/other.ts
+++ packages/safety-suite-api/src/legacyApi/other.ts
@@ -3,6 +3,11 @@ import momentTimezone from 'moment-timezone';
 import {Id} from '../types';
 import {LegacyApi} from './types';
 
+export const getApi: LegacyApi = {
+  method: 'GET',
+  route: '/api'
+};
+
 export function getCorrectiveActionLetterParams(
   correctiveActionId: Id
 ): LegacyApi {
