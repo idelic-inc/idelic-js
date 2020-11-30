00629ed9423c6ff1fca1302a1088b44fb2155d58
NickVarnado <nickvarnado@gmail.com>
2020-11-30 14:57:59 -0500

    added eforms export in index

--- packages/safety-suite-api/src/api/index.ts
+++ packages/safety-suite-api/src/api/index.ts
@@ -1,6 +1,7 @@
 export * from './authentication';
 export * from './configuration';
 export * from './customers';
+export * from './eforms';
 export * from './models';
 export * from './users';
 export * from './documentLibrary';
