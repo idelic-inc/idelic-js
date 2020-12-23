ab245de6642a54ed72435e69e673ec02d865e6c8
maxswa <mkswartwout@gmail.com>
2020-12-23 13:45:41 -0500

    Add updateIntegrationDate, update getIntegrations.

--- packages/safety-suite-api/src/api/integrations.ts
+++ packages/safety-suite-api/src/api/integrations.ts
@@ -10,12 +10,27 @@ export interface Integration {
 
 export function getIntegrations(
   customerAlias: string,
+  labels: string[] = [],
   apiOptions: ApiOptions = {}
 ): Request<ApiResponse<Integration[]>> {
   return runApi({
     method: 'GET',
     urlRoot: 'loginUrlRoot',
     route: `/api/1.0/integrations/${customerAlias}`,
+    requestOptions: {query: {labels}},
+    apiOptions
+  });
+}
+
+export function updateIntegrationDate(
+  customerAlias: string,
+  integrationAlias: string,
+  apiOptions: ApiOptions = {}
+): Request<ApiResponse<Integration[]>> {
+  return runApi({
+    method: 'PUT',
+    urlRoot: 'loginUrlRoot',
+    route: `/api/1.0/integrations/${customerAlias}/${integrationAlias}/touch`,
     apiOptions
   });
 }
