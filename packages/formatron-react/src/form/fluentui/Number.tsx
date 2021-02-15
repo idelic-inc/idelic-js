3bb5c554f5f1219c89f580d001d56a1997e00aa5
maxswa <mkswartwout@gmail.com>
2021-02-15 14:46:37 -0500

    Add Number field component.

---
+++ packages/formatron-react/src/form/fluentui/Number.tsx
@@ -0,0 +1,43 @@
+import {ITextFieldProps, TextField} from '@fluentui/react/lib/TextField';
+import {NumberType} from '@idelic/formatron/lib/dataTypes/number';
+import React from 'react';
+
+import {FieldObjectDataType} from '../Form';
+import {CommonFieldProps} from '../types';
+
+export interface NumberProps extends CommonFieldProps {
+  field: FieldObjectDataType<NumberType>;
+  textFieldProps?: ITextFieldProps;
+}
+
+export const Number: React.FC<NumberProps> = ({
+  field,
+  textFieldProps = {},
+  disabled = false,
+  label
+}) => {
+  const {
+    value,
+    setValue,
+    error,
+    dataType,
+    setTouched,
+    isTouched,
+    isRequired,
+    isSubmitting
+  } = field;
+
+  return (
+    <TextField
+      value={value ?? ''}
+      type="number"
+      onChange={(_, val) => setValue(val ? global.Number(val) : undefined)}
+      label={label ?? dataType.label}
+      disabled={disabled || isSubmitting}
+      required={isRequired}
+      onBlur={() => setTouched(true)}
+      errorMessage={isTouched ? error : undefined}
+      {...textFieldProps}
+    />
+  );
+};
--- packages/formatron-react/src/form/fluentui/index.ts
+++ packages/formatron-react/src/form/fluentui/index.ts
@@ -1,13 +1,19 @@
 import {Boolean} from './Boolean';
 import {DateTime} from './DateTime';
+import {Enum} from './Enum';
+import {Number} from './Number';
 import {Text} from './Text';
 
 export * from './Boolean';
 export * from './DateTime';
+export * from './Enum';
+export * from './Number';
 export * from './Text';
 
 export const fieldComponents = {
   boolean: Boolean,
   dateTime: DateTime,
+  enum: Enum,
+  number: Number,
   text: Text
 };
