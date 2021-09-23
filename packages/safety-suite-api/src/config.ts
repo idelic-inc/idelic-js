0619869298ce13dc9f214444d1ad87a30c031b42
NickVarnado <nickvarnado@gmail.com>
2021-09-23 09:28:09 -0400

    added hr

--- packages/safety-suite-api/src/config.ts
+++ packages/safety-suite-api/src/config.ts
@@ -232,8 +232,10 @@ export interface BasicConfig extends NestedConfiguration {
       };
       frontendUrl: string;
       modules: {
-        licenseStatus: {
-          enabled: boolean;
+        hr: {
+          licenseStatus: {
+            enabled: boolean;
+          };
         };
         safetyAndRisk: {
           severityPreventabilityVersion: number;
@@ -290,8 +292,10 @@ const exampleConfig: BasicConfig = {
       },
       frontendUrl: '',
       modules: {
-        licenseStatus: {
-          enabled: false
+        hr: {
+          licenseStatus: {
+            enabled: false
+          }
         },
         safetyAndRisk: {
           severityPreventabilityVersion: 0
