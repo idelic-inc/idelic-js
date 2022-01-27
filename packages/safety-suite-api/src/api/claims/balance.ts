f7d53ce68658eda90edd35e52da8d55f65b96bf2
Andrew Maltsev <andrew.maltsev@targsoftware.com>
2022-01-27 17:59:48 +0300

    balance for claim

---
+++ packages/safety-suite-api/src/api/claims/balance.ts
@@ -0,0 +1,38 @@
+import {Request} from '@idelic/safety-net';
+import {ApiOptions, ApiResponse, ApiSuccessResponse} from '../../types';
+import {runApi} from '../../runApi';
+
+interface Balance {
+  category: string;
+  reserve: number;
+  payment: number;
+  reimbursement: number;
+  incurredExpense: number;
+  reservesRemaining: number;
+}
+
+/**
+ * Get Balance For Claim By ClaimId.
+ *
+ * @param claimId - Claim Id.
+ * @param customerAlias - Customer alias.
+ * @param apiOptions - Optional options for runApi.
+ * @returns - Balance Array Grouped By Category.
+ */
+export function getBalanceByClaimId(
+  claimId: string,
+  customerAlias: string,
+  apiOptions?: ApiOptions
+): Request<ApiResponse<ApiSuccessResponse<Balance[]>>> {
+  return runApi({
+    method: 'GET',
+    urlRoot: 'claimsSinkUrlRoot',
+    route: `/api/balance/${claimId}`,
+    apiOptions,
+    requestOptions: {
+      query: {
+        customerAlias
+      }
+    }
+  });
+}
