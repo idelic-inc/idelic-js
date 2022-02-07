b0281d9b11792708b32c57ab4220a607ea602056
maxswa <mkswartwout@gmail.com>
2022-02-07 09:28:47 -0500

    Fix `getDatasetMeta` response.

--- packages/safety-suite-api/src/api/dashboardSink/meta.ts
+++ packages/safety-suite-api/src/api/dashboardSink/meta.ts
@@ -1,13 +1,13 @@
 import {Request} from '@idelic/safety-net';
 
 import {runApi} from '../../runApi';
-import {ApiOptions, ApiSuccessResponse} from '../../types';
+import {ApiOptions} from '../../types';
 import {DatasetMetaQuery, DatasetMetaResponse} from './types';
 
 export const getDatasetMeta = (
   query: DatasetMetaQuery,
   apiOptions: ApiOptions = {}
-): Request<ApiSuccessResponse<DatasetMetaResponse>> =>
+): Request<DatasetMetaResponse> =>
   runApi({
     method: 'GET',
     urlRoot: 'dashboardSinkUrlRoot',
