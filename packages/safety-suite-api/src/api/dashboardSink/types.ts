cf6fa25c972f425b09e96b3586feaf0217b14ddc
NickVarnado <nickvarnado@gmail.com>
2022-06-13 11:30:16 -0400

    add filter priority weight

--- packages/safety-suite-api/src/api/dashboardSink/types.ts
+++ packages/safety-suite-api/src/api/dashboardSink/types.ts
@@ -174,6 +174,8 @@ export interface AttributeMeta {
   alias: string;
   type: AttributeType;
   filterPriority?: number;
+  // Used for relations
+  filterPriorityWeight?: number;
   filterable: boolean;
   groupable: boolean;
   aggregations: KpiAggregation[];
