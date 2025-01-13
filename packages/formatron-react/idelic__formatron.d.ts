485575042a944e3d9a1b3ade8202192d86beff1f
NickVarnado <nickvarnado@gmail.com>
2025-01-13 09:38:13 -0500

    trying stuff

---
+++ packages/formatron-react/idelic__formatron.d.ts
@@ -0,0 +1,14 @@
+declare module '@idelic/formatron' {
+  export interface DataInterface {
+    // Define the properties and methods of DataInterface
+  }
+
+  export interface DataType {
+    // Define the properties and methods of DataType
+  }
+
+  export class FormatronClass {
+    constructor(dataInterface: DataInterface, dataTypes?: DataType[]);
+    // Define the properties and methods of Formatron
+  }
+}
